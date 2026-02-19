b2ac01a0d32d84bc749cfdec00ec390349372c03
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2026-02-19 22:17:15 +0530

    Comleted form validation and error handling of the form

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+const { wrap } = require("module");
+const { error } = require("console");
+const { listingSchema } = require("./schema");
 
 
 
@@ -29,41 +34,29 @@ async function main(params) {
     await mongoose.connect(MONGO_URL)
 }
 
-app.listen(port, () => {
-    console.log(`The app is listening on the port : ${port}`);
+
+// root route
+app.get("/", (req, res) => {
+    res.send('<a href="/listings"><button>Go to Listing</button></a>');
 });
 
-app.get("/", (req, res) => {
-    res.send('Hey Guys this is the home page of my major project');
-})
 
-// app.get("/testlisting", async (req, res) => {
-//     try {
-//         let sampleListing = new Listing({
-//             title: "My new Villa",
-//             description: "This is a great villa which offer you better services",
-//             image: "",
-//             price: 15264,
-//             location: "NewYork City, USA",
-//             country: "United State America"
-//         })
-
-//         const saveListing = await sampleListing.save();
-//         console.log(saveListing);
-
-//         res.send("Successfully testing ");
-//     }catch (err) {
-//         console.log(err);
-//         res.status(500).send("Error saving listing");
-//     }
-// });
-
+//listing validation function
+const validateListing = (req, res, next)=>{
+    const {error} = listingSchema.validate(req.body);
+    const errMsg = error.details.map((el)=> el.message).join(",");
+    if(error){
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", validateListing, wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index", { allListings });
-})
+}));
 
 //New Route
 app.get("/listings/new", (req, res) => {
@@ -71,41 +64,65 @@ app.get("/listings/new", (req, res) => {
 })
 
 //Show Route 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params;
     const details = await Listing.findById(id);
+    if (!details) {
+        return next(new ExpressError(404, "Listing not found"));
+    }
     // console.log(details);
     res.render("listings/show", { details });
-})
+}));
 
 //create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", validateListing,  wrapAsync(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
+    }
     // const {title, description, image, price, country, location} = req.body;
     const newListing = new Listing(req.body.listing);
+
     await newListing.save();
     res.redirect("/listings");
-})
+}));
 
 //Edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", validateListing, wrapAsync(async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
 
     res.render("listings/edit", { listing });
-})
+}));
 
 //update route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+        next(new ExpressError(400, "Send valid data for listing"));
+    }
     const { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-})
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     const { id } = req.params;
     const deletedItem = await Listing.findByIdAndDelete(id);
     console.log(deletedItem);
     res.redirect("/listings");
-})
+}));
 
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"))
+});
+
+
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Something went wrong" } = err;
+    res.render("error", { status, message });
+});
+
+app.listen(port, () => {
+    console.log(`The app is listening on the port : ${port}`);
+});
